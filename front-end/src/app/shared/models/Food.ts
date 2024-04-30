16b1a9b84f129a7b6755f451a63b163b8a90648f
yotr <lya325440@gmail.com>
2024-04-30 15:15:12 +0300

    models

---
+++ front-end/src/app/shared/models/Food.ts
@@ -0,0 +1,12 @@
+export class Food {
+id!: string;
+name!: string;
+price!: number;
+tags?: string[];
+favorite!:boolean;
+stars!: number;
+views!:number;
+imageUrl!: string;
+origins!: string[];
+cookTime!: string;
+}
