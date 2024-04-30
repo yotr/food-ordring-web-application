2fe7fd6d1e55e427a9c7f98315edacb11d132a04
yotr <lya325440@gmail.com>
2024-04-30 15:18:55 +0300

    auth gaurd

---
+++ front-end/src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router,private userServices:UserService) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.userServices.user.email) return true;
+      this.router.navigate(['/login']);
+      return false;
+  }
+  
+}
